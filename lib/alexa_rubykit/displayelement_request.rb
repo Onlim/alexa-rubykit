6e5851c18bae212f0db12295ce55142065696aac
Benny Klotz <benny.klotz92@gmail.com>
2019-06-26 12:39:51 +0200

    Proper formatting

--- lib/alexa_rubykit/displayelement_request.rb
+++ lib/alexa_rubykit/displayelement_request.rb
@@ -2,10 +2,7 @@ module AlexaRubykit
   class DisplayelementRequest < IntentRequest
     attr_accessor :intent, :name, :slots
 
-    # We still don't know if all of the parameters in the request are required.
-    # Checking for the presence of intent on an IntentRequest.
     def initialize(json_request)
-
       json_request['request']['intent'] = {
         'name' => "Display.ElementSelected",
         'slots' => {
@@ -18,10 +15,7 @@ module AlexaRubykit
         }
       }
 
-      #than calling intent request constructor
       super
-
     end
-
   end
 end
